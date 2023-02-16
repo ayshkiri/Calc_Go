d46949304aacf7bc8ef91c885b4b414709f56c27
ayshkiri <alexshkirin@mail.ru>
2023-02-16 15:43:10 +0300

    Add more better solution

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func RomanToInteger(romanNumber string) (int, error) {
@@ -21,90 +24,78 @@ func RomanToInteger(romanNumber string) (int, error) {
 	if romanNumbers[romanNumber] != 0 {
 		return romanNumbers[romanNumber], nil
 	} else {
-		err := errors.New("IncorrectRomanNumber")
+		err := errors.New("Некорректное римское число от 1 (I) до 10 (X)")
 		return 0, err
 	}
 }
 
-func IntegerToRoman(number int) (string, error) {
-	var res string
-	if number >= 1 && <= 10{
-		tenCount := number / 10
-		if tenCount == 10 {
-			res += "C"
+func IntegerToRoman(number int) string {
+	var result string
+	romanNumbers := []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	arabicNumbers := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
+	for i := 0; number > 0; i++ {
+		for arabicNumbers[i] <= number {
+			result += romanNumbers[i]
+			number -= arabicNumbers[i]
 		}
-		if tenCount == 9 {
-			res += "XC"
-		}
-		if tenCount >= 5 && tenCount < 9 {
-			res += "L"
-			for i := 1; i <= tenCount-5; i++ {
-				res += "X"
-			}
-		}
-		if tenCount >= 1 && tenCount < 5 {
-			for i := 1; i <= tenCount; i++ {
-				res += "X"
-			}
-		}
-		if number%10 != 0 {
-			ostatok := number % 10
-			if ostatok == 9 {
-				res += "IX"
-			}
-			if ostatok >= 5 && ostatok < 9 {
-				res += "V"
-				for i := 1; i <= ostatok-5; i++ {
-					res += "I"
-				}
-			}
-			if ostatok >= 1 && ostatok < 5 {
-				for i := 1; i <= ostatok; i++ {
-					res += "I"
-				}
-			}
-		}
-		return res, nil
-	} else {
-		err := errors.New("IncorrectArabicNumber")
-		return "", err
 	}
+	return result
 }
 
 func main() {
-	var numOneStr, numTwoStr, operator string
-	fmt.Scan(&numOneStr, &operator, &numTwoStr)
-
-	numOneInt, errOne := strconv.Atoi(numOneStr)
-	if errOne != nil {
-		fmt.Println("Первый аргумент не является числом")
+	reader := bufio.NewReader(os.Stdin)
+	inputData, _ := reader.ReadString('\n')
+	inputData = strings.Trim(inputData, "\n")
+	var inputStrArray []string = strings.Split(inputData, " ")
+	if len(inputStrArray) != 3 {
+		fmt.Println("Некорректные входные данные")
+		return
 	}
-
-	fmt.Println("Первое число:", numOneStr, numOneInt)
-
-	numTwoInt, errTwo := RomanToInteger(numTwoStr)
-	if errTwo != nil {
-		fmt.Println("Такого римского числа нет")
+	operators := map[string]int{"+": 1, "-": 1, "*": 1, "/": 1}
+	_, check := operators[inputStrArray[1]]
+	if !check {
+		fmt.Println("Некорректные входные данные")
+		return
 	}
-	fmt.Println("Второе число: ", numTwoStr, numTwoInt)
+	numberOne, errNumberOne := strconv.Atoi(inputStrArray[0])
+	numberTwo, errNumberTwo := strconv.Atoi(inputStrArray[2])
+	if errNumberOne == nil && errNumberTwo == nil && numberOne >= 1 && numberOne <= 10 && numberTwo >= 1 && numberTwo <= 10 {
+		// Возвращаемая функцией Calc ошибка пропускается, т.к. в условии выше уже есть проверка,
+		// что передаваемые аргументы больше 0
+		arabicResult, _ := Calc(numberOne, numberTwo, inputStrArray[1])
+		fmt.Println(arabicResult)
+		return
+	}
+	romanNumberOne, errRomanNumberOne := RomanToInteger(inputStrArray[0])
+	romanNumberTwo, errRomanNumberTwo := RomanToInteger(inputStrArray[2])
+	if errRomanNumberOne == nil && errRomanNumberTwo == nil && romanNumberOne > romanNumberTwo {
+		romanResult, _ := Calc(romanNumberOne, romanNumberTwo, inputStrArray[1])
+		fmt.Println(IntegerToRoman(romanResult))
+		return
+	}
+	fmt.Println("Некорректные входные данные")
+}
 
-	//var result int
-	//switch operator {
-	//case "+":
-	//	result = numOne + numTwo
-	//	fmt.Println(result)
-	//case "-":
-	//	result = numOne - numTwo
-	//	fmt.Println(result)
-	//case "*":
-	//	result = numOne * numTwo
-	//	fmt.Println(result)
-	//case "/":
-	//	if numTwo != 0 {
-	//		result = numOne / numTwo
-	//		fmt.Println(result, len(operator))
-	//	} else {
-	//		fmt.Println("Делить на 0 нельзя!")
-	//	}
-	//}
+func Calc(numOne, numTwo int, operator string) (int, error) {
+	var result int
+	switch operator {
+	case "+":
+		result = numOne + numTwo
+		return result, nil
+	case "-":
+		result = numOne - numTwo
+		return result, nil
+	case "*":
+		result = numOne * numTwo
+		return result, nil
+	case "/":
+		if numTwo != 0 {
+			result = numOne / numTwo
+			return result, nil
+		} else {
+			err := errors.New("Делить на 0 нельзя")
+			return 0, err
+		}
+	}
+	return 0, nil
 }
